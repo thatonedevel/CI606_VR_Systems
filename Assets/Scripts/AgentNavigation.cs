b081d7115556deb49ba6a5c829f57412e27d5eca
Nora Dwelly <dwelly.412@gmail.com>
2025-10-26 16:25:55 +0000

    Added Idle and wander state methods

--- Assets/Scripts/AgentNavigation.cs
+++ Assets/Scripts/AgentNavigation.cs
@@ -6,11 +6,15 @@ public class AgentNavigation : MonoBehaviour
 {
     private bool atTarget = false;
     private int patrolIndex = 0;
+    private float idleStartTime = 0.0f;
+    private bool isIdle = false;
+
 
     [Header("Navigation Info")]
     [SerializeField] private NavMeshAgent agent;
     [SerializeField] private List<Transform> patrolPoints = new List<Transform>();
     [SerializeField] private float distanceThreshold = 1.0f;
+    [SerializeField] private float idleDuration = 5.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -41,6 +45,58 @@ public class AgentNavigation : MonoBehaviour
             agent.SetDestination(patrolPoints[patrolIndex].position);
             atTarget = false;
         }
+    }
+
+    void Idle()
+    {
+        if (!isIdle)
+        {
+            // agent is not idle yet
+            // disable navigation
+            agent.enabled = false;
+            // set idle start time
+            idleStartTime = Time.time;
+            isIdle = true;
+        }
+        else
+        {
+            // check if idle duration is elapsed
+            if (Time.time - idleStartTime >= idleDuration)
+            {
+                // move to another state
+                agent.enabled = true;
+                isIdle = false;
+            }
+        }
+    }
+
+    void Wander()
+    {
+        // wondering - move to a location, pick a random direction, then move
+        agent.enabled = true;
+
+        // check distance to target
+        if ((agent.destination - transform.position).magnitude <= distanceThreshold)
+        {
+            // at target, pick random direction
+            int randomX = Random.Range(-10, 10);
+            int randomZ = Random.Range(-10, 10);
+            Vector3 randomDirection = new Vector3(randomX, 0, randomZ);
+
+            // check if random direction is valid on navmesh
+            var dest = transform.position + randomDirection;
+
+            if (agent.CalculatePath(dest, new NavMeshPath()))
+            {
+                // it's a valid path
+                agent.SetDestination(dest);
+            }
+        }
+    }
+
+    void ManageStates()
+    {
+        // logic for managing states here
     }
 }
 
