34e1630af37705e0e9a68e49b70b0f4256872cb0
Nora Dwelly <dwelly.412@gmail.com>
2026-01-18 14:54:44 +0000

    AI is fixed and added spawner

--- Assets/Scripts/VenueExit.cs
+++ Assets/Scripts/VenueExit.cs
@@ -1,8 +1,11 @@
+using System;
 using UnityEngine;
 
 public class VenueExit : MonoBehaviour
 {
     // Navigable object that destroys agents on detection
+
+    public static event Action OnAgentDestroyed;
 
     private void OnTriggerEnter(Collider other)
     {
