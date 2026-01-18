70d07d5edc43d4889905180d4ceb21f681782ca5
MacoronieTheBonnonie <232444287+MacoronieTheBonnonie@users.noreply.github.com>
2026-01-18 03:56:06 +0000

    The UI Works.
    The UI has been implemented as requsted

--- Assets/Scripts/MT_DisplayTheUIonButtonPress.cs
+++ Assets/Scripts/MT_DisplayTheUIonButtonPress.cs
@@ -3,26 +3,31 @@ using UnityEngine.InputSystem;
 
 public class MT_DisplayTheUIonButtonPress : MonoBehaviour
 {
-    public InputActionProperty teastActionValue;
+    //Learned how to make custom input for VR in this tutorial 
+        //https://youtu.be/IVkQL-ECeN4?si=EiH7lQxJtdntwOTg
     public InputActionProperty testActionButton;
     [SerializeField] GameObject holder;
-    private bool buttonPressed = false;
+   
 
 
-    private void OnEnable()
+    
+    void Start()
     {
-        testActionButton.action.Enable();
         testActionButton.action.performed += MenuShow;
     }
 
-    private void OnDisable()
+    private void OnDestroy()
     {
         testActionButton.action.performed -= MenuShow;
-        testActionButton.action.Disable();
     }
 
-    private void MenuShow(InputAction.CallbackContext callback)
+    //Similiar to MT_TeleportationActivator class Action_performed method
+    private void MenuShow(InputAction.CallbackContext obj)
     {
+        //Basicly this inverts the current visiability of the layer when the button is pressed
+        //The documentation for activeSelf, basicly it shows the current activity of the object if active it is true, if it is false it is fasle
+            //https://docs.unity3d.com/6000.3/Documentation/ScriptReference/GameObject-activeSelf.html
+        //SetActive is very commonly used
         holder.SetActive(!holder.activeSelf);
     }
 }
---
+++ Assets/Scripts/MT_UI_Manager.cs
@@ -0,0 +1,86 @@
+//using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MT_UI_Manager : MonoBehaviour
+{
+    [SerializeField] string lobbySceneName;
+    [SerializeField] string simulationSceneName;
+    [SerializeField] public GameObject startScreen;
+    [SerializeField] public GameObject pauseScreen;
+    [SerializeField] public TextMeshProUGUI timeText;
+    private bool timeStoped = false;
+
+    //Used to find how to check what scene you currently are in
+    //https://discussions.unity.com/t/how-to-check-which-scene-is-loaded-and-write-if-code-for-it/163399/2
+    void Start()
+    {
+        // Create a temporary reference to the current scene.
+        UnityEngine.SceneManagement.Scene currentScene = SceneManager.GetActiveScene();
+
+        // Retrieve the name of this scene.
+        string sceneName = currentScene.name;
+
+        if (sceneName == lobbySceneName)
+        {
+            startScreen.SetActive(true);
+            pauseScreen.SetActive(false);
+        }
+        else if (sceneName == simulationSceneName)
+        {
+            startScreen.SetActive(false);
+            pauseScreen.SetActive(true);
+        }
+    }
+    //To freeze and unfreeze time Time.timeScale is set to 0 to freze and 1 to undo
+    //https://discussions.unity.com/t/how-to-freeze-and-unfreeze-my-game/311091
+    public void StopTimeButton()
+    {
+        if (timeStoped == false)
+        {
+            Time.timeScale = 0;
+            timeStoped = true;
+            timeText.text = "Unfreeze Time";
+        }
+        else if (timeStoped == true)
+        {
+            Time.timeScale = 1;
+            timeStoped = false;
+            timeText.text = "Freeze Time";
+        }
+    }
+    
+    //Switch scenes
+    public void SwitchSceneToSimiluation()
+    {
+        //Ensuring that the time is no longer frozen before the scene swap
+        TimeUnfreze();
+        //This is how to load scene
+        //https://discussions.unity.com/t/how-to-switch-between-scenes/189643
+        SceneManager.LoadScene(simulationSceneName);
+    }
+    public void SwitchSceneToLobby()
+    {
+        TimeUnfreze();
+        SceneManager.LoadScene(lobbySceneName);
+    }
+    //This is how to quit the application only works when the game is built. 
+    //MT_InteractableHands branch was built and tested and it works
+    //https://docs.unity3d.com/6000.3/Documentation/ScriptReference/Application.Quit.html
+    public void QuitButton()
+    {
+        TimeUnfreze();
+        Application.Quit();
+    }
+
+    //Ensuring that time is unfrezed before the scene change
+    private void TimeUnfreze()
+    {
+        //Set to true so it will unfreze it and reset the text
+        timeStoped = true;
+        StopTimeButton();
+    }
+    
+}
+
